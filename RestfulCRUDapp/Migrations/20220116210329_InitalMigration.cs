68b05c88a5e8affae00786b08e4c3236b2174192
David <>
2022-01-16 23:32:20 +0000

    Connect to local DB/Migration. Tidying endpoints
    WIP

---
+++ RestfulCRUDapp/Migrations/20220116210329_InitalMigration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestfulCRUDapp.Migrations
+{
+    public partial class InitalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TeamName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
--- RestfulCRUDapp/Program.cs
+++ RestfulCRUDapp/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using RestfulCRUDapp.Models;
 using RestfulCRUDapp.PlayerData;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -8,7 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IPlayerData, FakePlayerData>();
+
+builder.Services.AddDbContextPool<PlayerContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("PlayerContextConnectionString")));
+
+builder.Services.AddScoped<IPlayerData, SqlPlayerData>();
 
 var app = builder.Build();
 
